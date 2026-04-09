4efef161450667131bba1fd743b057983bc8b0f1
httpmarco <httpmarco@gmx.de>
2026-04-09 20:38:27 +0200

    Add core classes for dependency resolution and checksum computation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
+    `maven-publish`
+    signing
+
+    alias(libs.plugins.nexus.publish)
 }
 
 group = "de.polocloud"
@@ -13,11 +17,48 @@ repositories {
 
 gradlePlugin {
     plugins {
-        create("polocloudDependencyPlugin") {
-            id = "de.polocloud.dependency"
+        create("polocloudGradlePlugin") {
+            id = "de.polocloud.gradle.plugin"
             implementationClass = "de.polocloud.gradle.plugin.PolocloudPlugin"
             displayName = "Polocloud Gradle Plugin"
             description = "Development tool for every polocloud component"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            if (project.plugins.hasPlugin("org.jetbrains.kotlin.jvm")) {
+                from(components["java"])
+
+                pom {
+                    name.set("polocloud-gradle-plugin")
+                    description.set("Dynamic translation system for the PoloCloud ecosystem.")
+                    url.set("https://github.com/thePolocloud/polocloud-gradle-plugin")
+
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("httpmarco")
+                            name.set("Mirco Lindenau")
+                            email.set("business-ml@gmx.de")
+                        }
+                    }
+
+                    scm {
+                        url.set("https://github.com/thePolocloud/polocloud-gradle-plugin")
+                        connection.set("scm:git:https://github.com/thePolocloud/polocloud-gradle-plugin.git")
+                        developerConnection.set("scm:git:https://github.com/thePolocloud/polocloud-gradle-plugin.git")
+                    }
+                }
+            }
         }
     }
 }
@@ -30,4 +71,27 @@ dependencies {
 
 kotlin {
     jvmToolchain(25)
+}
+
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+
+            username.set(System.getenv("OSSRH_USERNAME"))
+            password.set(System.getenv("OSSRH_PASSWORD"))
+        }
+    }
+}
+
+signing {
+    val signingKey = System.getenv("GPG_PRIVATE_KEY")
+    val signingPassphrase = System.getenv("GPG_PASSPHRASE")
+
+    if (signingKey != null && signingPassphrase != null) {
+        useInMemoryPgpKeys(signingKey, signingPassphrase)
+        sign(publishing.publications)
+    }
 }
